ee58602d83b3636482d110d7fc89264eb3560eed
mich0w0h <aokimiho71@gmail.com>
2024-03-04 05:52:55 +0900

    chore: update astro v2 to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
+	integrations: [tailwind(), mdx()],
 });
